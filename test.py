393ed2f00b534290a9216f2816b45ba97488bb0c
oxotnik44 <artemnovikov1969@gmail.com>
2023-01-30 12:07:34 +0700

    Готово

--- test.py
+++ test.py
@@ -7,6 +7,11 @@ from kivy.core.window import Window
 
 Window.size = (360, 800)
 
+def on_press(btn, textinput, label, colors_dict, i):
+    textinput.text = ""
+    textinput.insert_text(btn)
+    label.text = ""
+    label.text = colors_dict[i]
 
 class MyApp(App):
     def build(self):
@@ -16,7 +21,7 @@ class MyApp(App):
         colors_dict = ["красный", "оранжевый", "желтый",
                        "зеленый", "голубой", "синий", "фиолетовый"]
         textinput = TextInput(size_hint=(1, .1), pos_hint={
-                              'center_x': .5, 'center_y': .75})
+            'center_x': .5, 'center_y': .75})
         layout.add_widget(textinput)
         label = Label(text='Enter your text here:', size_hint=(
             1, .1), pos_hint={'center_x': .5, 'center_y': .9})
@@ -24,11 +29,10 @@ class MyApp(App):
         for i in range(7):
             button = Button(text=str(colors[i]), background_color=colors[i], size_hint=(1, .1),
                             pos_hint={'center_x': .5, 'center_y': .65 - i*.1})
-            button.bind(on_press=lambda x, btn=colors[i]: (textinput.text="", textinput.insert_text(btn), label.text="", label.text=colors_dict[i]))
-
+            button.bind(on_press=lambda x, btn=colors[i], textinput=textinput, label=label, colors_dict=colors_dict, i=i: on_press(
+                btn, textinput, label, colors_dict, i))
             layout.add_widget(button)
         return layout
-
 
 if __name__ == '__main__':
     MyApp().run()
